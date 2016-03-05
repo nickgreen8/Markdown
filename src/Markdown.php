cb6f4d7d2934a576d0668925039fe8319a6fbc57
Nick Green <nick@nickgreenweb.co.uk>
2016-03-05 14:34:35 +0000

    Initial Setup

---
+++ src/Markdown.php
@@ -0,0 +1,20 @@
+<?php
+namespace N8G\Markdown;
+
+/**
+ * This class is to be used to implement a markdown parser. Even though there are a number of different variations of
+ * this kind of tool, the aim is to solve some common issues that I can utilise within my own projects.
+ *
+ * @author Nick Green <nick@nick8green.co.uk>
+ */
+class Markdown
+{
+	/**
+	 * Parses text into HTML for display.
+	 *
+	 * @param  string $text The text to be processed.
+	 * @return string       The text passed as HTML.
+	 */
+	public function parse($text)
+	{}
+}
---
+++ tests/MarkdownTests.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tests;
+
+use N8G\Markdown\Markdown;
+
+class MarkdownTests extends \PHPUnit_Framework_TestCase
+{
+}
